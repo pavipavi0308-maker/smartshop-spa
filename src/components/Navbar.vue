37475e2e03bd00ffffe88561c4bd767ce6d8ece0
pavipavi0308-maker <pavipavi0308@gmail.com>
2026-03-05 00:30:23 +0530

    Improved navbar cart badge UI

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,8 +15,23 @@ const router = useRouter()
 SmartShop
 </h2>
 
-    <div @click="router.push('/cart')" style="cursor:pointer;">
-  🛒 Cart ({{ cart.length }})
+   <div @click="router.push('/cart')" style="cursor:pointer; position:relative;">
+
+  🛒 Cart
+
+  <span
+  v-if="cart.length > 0"
+  style="
+  background:red;
+  color:white;
+  padding:2px 6px;
+  border-radius:50%;
+  font-size:12px;
+  margin-left:5px;
+  ">
+  {{ cart.length }}
+  </span>
+
 </div>
 
   </div>
