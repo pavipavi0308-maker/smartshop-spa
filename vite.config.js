27f40d596e7ed566ea2dc84c00ca95a94c25abd9
pavipavi0308-maker <pavipavi0308@gmail.com>
2026-05-22 00:51:39 +0530

    Clean up generated config files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-// https://vite.dev/config/
-export default defineConfig({
-    base: process.env.GITHUB_PAGES === "true" ? "/smartshop-spa/" : "/",
-    plugins: [vue()],
-});
